19dfc6bbe5550f29e856f3492656820c6049769c
PasinduAnjana <pasinduanjana000@gmail.com>
2024-02-23 18:39:45 +0530

    changed dashboard layout

--- app/(protected)/layout.tsx
+++ app/(protected)/layout.tsx
@@ -10,7 +10,6 @@ const ProtectedLayout = ({ children }: ProtectedLayoutProps) => {
     <div className="h-full w-full flex flex-col gap-y-10 items-center justify-center bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-neutral-800 to-neutral-950">
       {/* <Navbar /> */}
 
-      <UserButton />
       {children}
     </div>
   );
